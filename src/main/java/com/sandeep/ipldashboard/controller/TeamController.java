2f80b893ae6dc48165cdebf88d398556cf8d8755
Sandeep Prajapati <sandeep141195@gmail.com>
2021-06-18 23:09:31 +0530

    Added total match played by team in any year API

--- src/main/java/com/sandeep/ipldashboard/controller/TeamController.java
+++ src/main/java/com/sandeep/ipldashboard/controller/TeamController.java
@@ -1,5 +1,6 @@
 package com.sandeep.ipldashboard.controller;
 
+import com.sandeep.ipldashboard.model.Match;
 import com.sandeep.ipldashboard.model.Team;
 import com.sandeep.ipldashboard.repository.MatchRepository;
 import com.sandeep.ipldashboard.repository.TeamRepository;
@@ -7,6 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -22,10 +26,17 @@ public class TeamController {
 
     @GetMapping("/teams/{teamName}")
     public Team getTeam(@PathVariable String teamName) {
-        LOGGER.info("Request for Team :{}", teamName);
+        LOGGER.info("Request for Matches played by team :{}", teamName);
         Team response = teamRepository.getTeamByTeamName(teamName);
         response.setMatches(matchRepository.getLatestMatchByTeam(teamName, 4));
-        LOGGER.info("Response for Team :{}", response);
         return response;
+    }
+
+    @GetMapping("/teams/{teamName}/matches")
+    public List<Match> getMatchForTeam(@PathVariable String teamName, @RequestParam int year){
+        LOGGER.info("Request for Match played by team : {} in year : {}", teamName, year);
+        LocalDate startDate = LocalDate.of(year,1,1);
+        LocalDate endDate = LocalDate.of(year,12,31);
+        return matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
     }
 }
--- src/main/java/com/sandeep/ipldashboard/repository/MatchRepository.java
+++ src/main/java/com/sandeep/ipldashboard/repository/MatchRepository.java
@@ -3,9 +3,12 @@ package com.sandeep.ipldashboard.repository;
 import com.sandeep.ipldashboard.model.Match;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -13,6 +16,16 @@ public interface MatchRepository extends CrudRepository<Match, Long> {
 
     List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
 
+    /**
+     * We can also provide exact query (@Query Annotation) which should be run to JPA. so JPA will not create any query on the basis of
+     * method names;
+     */
+    @Query("select m from Match m where (m.team1 = :teamName or team2 =: teamName) and m.date between :sDate and :eDate")
+    List<Match> getMatchesByTeamBetweenDates(@Param("teamName") String teamName,
+                                             @Param("sDate") LocalDate sDate, @Param("eDate") LocalDate eDate);
+
+//    List<Match> getByTeam1OrTeam2AndDateBetweenOrderByDateDesc(@Param("teamName") String teamName, 
+//                                                               @Param("sDate") LocalDate sDate, @Param("eDate") LocalDate eDate);
     default List<Match> getLatestMatchByTeam(String teamName, int count){
         Pageable pageableRequest = PageRequest.of(0,count);
         return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, pageableRequest);
